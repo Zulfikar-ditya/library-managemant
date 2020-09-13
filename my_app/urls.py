833a5f269dbd9f2c4646ab6e278a426a874ea767
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2020-09-13 21:12:14 +0700

    member list, 404, success page

--- my_app/urls.py
+++ my_app/urls.py
@@ -6,5 +6,12 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # member
     path('add-member/', views.add_member, name='add-member'),
+    path('member-list/', views.member_list, name='member-list'),
+
+    # error success
+    path('404/', views.not_found, name='not_found'),
+    path('success/', views.success, name='success'),
+
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.core.paginator import Paginator
 
 from .models import Rating, Categorie, Book, Member, Peminjaman
 from .forms import MemberForm
@@ -21,7 +22,7 @@ def add_member(request):
                 instance.user = request.user
                 instance.save()
                 if request.POST['add']:
-                    return HttpResponseRedirect(reverse('home:index'))
+                    return HttpResponseRedirect(reverse('home:list-member'))
                 else:
                     return HttpResponseRedirect(reverse('home:add-member'))
                     
@@ -34,3 +35,26 @@ def add_member(request):
     else:
         return HttpResponseRedirect(reverse('login'))
 
+
+def member_list(request):
+    if request.user.is_authenticated:
+        page = 'Member'
+        data = Member.objects.all()
+        paginator = Paginator(data, 100)
+        pageNum = request.GET.get('page')
+        dataresult = paginator.get_page(pageNum)
+        return render(request, 'home/list.html', {
+            'page': page,
+            'data': dataresult,
+        })
+    else:
+        return HttpResponseRedirect(reverse('login'))
+        
+
+# succes error page
+def not_found(request):
+    return render(request, 'error-success/404.html')
+
+
+def success(request):
+    return render(request, 'error-success/success.html')
