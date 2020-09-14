f314c52af2d6fbf324583b1dffceaf6b8e35b55c
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2020-09-14 22:46:27 +0700

    search member by id and by contains name

--- my_app/views.py
+++ my_app/views.py
@@ -38,6 +38,13 @@ def add_member(request):
 
 def member_list(request):
     if request.user.is_authenticated:
+        if request.method == 'POST':
+            try:
+                query = request.POST['nama']
+                return search_member_by_name(request, query)
+            except:
+                query = request.POST['id']
+                return search_member_by_id(request, query)
         page = 'Member'
         data = Member.objects.all().order_by('id')
         paginator = Paginator(data, 100)
@@ -49,7 +56,38 @@ def member_list(request):
         })
     else:
         return HttpResponseRedirect(reverse('login'))
+
+
+def search_member_by_name(request, query):
+    if request.user.is_authenticated:
+        page = f'Search result for name: {query}'
+        data = Member.objects.filter(name__contains=query)
+        paginator = Paginator(data, 100)
+        pageNum = request.GET.get('page')
+        dataresult = paginator.get_page(pageNum)
+        return render(request, 'home/list.html', {
+            'page': page,
+            'data': dataresult,
+        })
+    else: 
+        return HttpResponseRedirect(reverse('login'))
         
+
+def search_member_by_id(request, query):
+    if request.user.is_authenticated:
+        page = f'Search result for id: {query}'
+        data = Member.objects.filter(pk=query)
+        paginator = Paginator(data, 100)
+        pageNum = request.GET.get('page')
+        dataresult = paginator.get_page(pageNum)
+        return render(request, 'home/list.html', {
+            'page': page,
+            'data': dataresult,
+        })
+    else: 
+        return HttpResponseRedirect(reverse('login'))
+        
+
 
 # succes error page
 def not_found(request):
